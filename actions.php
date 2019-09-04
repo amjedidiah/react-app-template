b1960c010a4554a72a1e6ad12357958ccc790373
aestart <aestartng@gmail.com>
2019-09-04 20:20:37 +0100

    trying to fix sendmail bug

--- actions.php
+++ actions.php
@@ -146,9 +146,9 @@
 
     try {
         $response = $sendgrid->send($email);
-        // print $response->statusCode() . "\n";
-        // print_r($response->headers());
-        // print $response->body() . "\n";
+        print $response->statusCode() . "\n";
+        print_r($response->headers());
+        print $response->body() . "\n";
     } catch (Exception $e) {
         // echo 'Caught exception: ',  $e->getMessage(), "\n";
         print("error");
